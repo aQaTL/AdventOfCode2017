753fe6e52f302222fe79c8afe73fc2a7c1eeec0a
aQaTL <mmsoltys@outlook.com>
2017-12-08 06:38:41 +0100

    Day 8

---
+++ day8/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	bytes, err := ioutil.ReadFile("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	input := strings.Split(string(bytes), "\n")
+
+	registers := make(map[string]int)
+
+	maxEver := -(1 << 31)
+
+	for _, line := range input {
+		instr := strings.Split(line, " ")
+
+		regName := instr[0]
+		mode := instr[1]
+		val, _ := strconv.Atoi(instr[2])
+
+		regInCondition := instr[4]
+		conditionVal, _ := strconv.Atoi(instr[6])
+
+		if mode == "dec" {
+			val *= -1
+		}
+
+		switch instr[5] {
+		case "==":
+			if registers[regInCondition] == conditionVal {
+				registers[regName] += val
+			}
+		case "!=":
+			if registers[regInCondition] != conditionVal {
+				registers[regName] += val
+			}
+		case ">":
+			if registers[regInCondition] > conditionVal {
+				registers[regName] += val
+			}
+		case ">=":
+			if registers[regInCondition] >= conditionVal {
+				registers[regName] += val
+			}
+		case "<":
+			if registers[regInCondition] < conditionVal {
+				registers[regName] += val
+			}
+		case "<=":
+			if registers[regInCondition] <= conditionVal {
+				registers[regName] += val
+			}
+		default:
+			fmt.Println("unimplemented instruction: ", instr[5])
+		}
+
+		if registers[regName] > maxEver {
+			maxEver = registers[regName]
+		}
+	}
+
+	max := -(1 << 31)
+	for _, v := range registers {
+		if v > max {
+			max = v
+		}
+	}
+
+	fmt.Println(max) //PartOne
+	fmt.Println(maxEver) //PartTwo
+}
